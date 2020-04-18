b09ddffb2a53c4abdb73575afd5f9d507769da08
JoeLowtech <ton.maier@gmail.com>
2020-04-18 21:35:16 +0200

    Implement compiling of nanopb

---
+++ messages/compileNanoPb.ps1
@@ -0,0 +1,4 @@
+﻿param([string]$compiled_file_name="",[string]$input_file="")
+
+..\coding\nanopb\generator\protoc.bat -o $compiled_file_name $input_file
+python ..\coding\nanopb\generator\nanopb_generator.py $compiled_file_name
