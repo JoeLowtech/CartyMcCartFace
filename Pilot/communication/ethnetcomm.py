6a2fe2ed3d56bcbb5997a97cf6d4828f99077ae3
JoeLowtech <ton.maier@gmail.com>
2020-05-05 22:51:03 +0200

    Adds length of msg to encoded msg

--- Pilot/communication/ethnetcomm.py
+++ Pilot/communication/ethnetcomm.py
@@ -3,6 +3,7 @@ import socketserver
 import socket
 import logging
 import time
+from google.protobuf.internal.encoder import _VarintBytes
 # import ifaddr
 
 from messages import messages_pb2
@@ -43,12 +44,12 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 
 if __name__ == "__main__":
     HOST, PORT = "192.168.178.36", 9999
-
     message = messages_pb2.driveMessage()
     message.steering = 1500
     message.power = 0.5
-    data_to_send = message.SerializeToString()
-
+    encoded_data = message.SerializeToString()
+    data_to_send = _VarintBytes(message.ByteSize()) + encoded_data
+    
     LOGGER.info(f"Connecting to {HOST}:{PORT}")
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         # Connect to server and send data
@@ -56,7 +57,7 @@ if __name__ == "__main__":
         LOGGER.info("Connected to server.")
         
         while True:
-            LOGGER.info("send data.")
+            LOGGER.info(f"send data: {data_to_send}")
             sock.sendall(data_to_send)
             time.sleep(1)
 
