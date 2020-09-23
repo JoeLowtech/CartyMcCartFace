8830b6417c85e6ad2be5fce9ea50180c81c2abf1
JoeLowtech <ton.maier@gmail.com>
2020-09-23 18:15:47 +0200

    Add default start value to motor/servo

--- Drive/FRDM-K66F-Drive-Controller/drive/Motor.h
+++ Drive/FRDM-K66F-Drive-Controller/drive/Motor.h
@@ -7,12 +7,14 @@ class Motor{
     static constexpr uint32_t MOTOR_SIGNAL_FREQUENCY = 500;
     static constexpr float MOTOR_MIN_RANGE = 0.3;
     static constexpr float MOTOR_MAX_RANGE = 0.9;
+    static constexpr float MOTOR_STOP = 0.4;
 
     private:
         PwmOut pwm;
     public:
         Motor(PinName pin): pwm(pin){
             pwm.period_ms(1000/MOTOR_SIGNAL_FREQUENCY);
+            pwm.write(MOTOR_STOP);
         }
         void set(float dutycylce){
             pwm.write(min(max(dutycylce,MOTOR_MIN_RANGE),MOTOR_MAX_RANGE));
--- Drive/FRDM-K66F-Drive-Controller/drive/Servo.h
+++ Drive/FRDM-K66F-Drive-Controller/drive/Servo.h
@@ -8,12 +8,15 @@ class Servo{
     static constexpr uint32_t SERVO_SIGNAL_FREQUENCY = 50;
     static constexpr uint32_t SERVO_MIN_RANGE = 1200;
     static constexpr uint32_t SERVO_MAX_RANGE = 1700;
+    static constexpr uint32_t SERVO_CENTER = 1450;
+
     private:
         PwmOut pwm;
 
     public:
         Servo(PinName pin):pwm(pin){
             pwm.period_ms(1000/SERVO_SIGNAL_FREQUENCY);
+            pwm.pulsewidth_us(SERVO_CENTER);
         }
 
         void set(uint32_t pulsewidth){
